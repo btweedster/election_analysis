ffccc603353a442f78c24617a1bc363381897ee3
btweedster <benjamin@Bens-Mac.local>
2022-10-29 01:00:08 -0400

    Analysis updated to include county data

--- PyPoll_Challenge.py
+++ PyPoll_Challenge.py
@@ -12,10 +12,18 @@ total_votes = 0
 candidate_options = []
 candidate_votes = {}
 
+# Initialize a county list and county votes dictionary
+county_list = []
+county_votes = {}
+
 # Declare winning candidate variables
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
+
+# Track the largest county and county voter turnout
+largest_county = ""
+largest_county_votes = 0
 
 # Open the .csv data file
 with open(file_to_load) as election_data:
@@ -35,6 +43,9 @@ with open(file_to_load) as election_data:
         # Get the candidate name from each row
         candidate_name = row[2]
 
+        # Get the county name from each row
+        county_name = row[1]
+
         # Add the candidate name to the candidate options if not already there
         if candidate_name not in candidate_options:
 
@@ -46,6 +57,18 @@ with open(file_to_load) as election_data:
         
         # Add vote to candidate count
         candidate_votes[candidate_name] += 1
+
+        # Check if county is in county list
+        if county_name not in county_list:
+            
+            # Add county name to county list
+            county_list.append(county_name)
+
+            # Add county name ot county votes
+            county_votes[county_name] = 0
+        
+        # Add vote to county votes
+        county_votes[county_name] += 1
     
     # Save the results to txt file
 with open(file_to_save,"w") as txt_file:
@@ -54,12 +77,46 @@ with open(file_to_save,"w") as txt_file:
         f"Election Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n"
+        f"-------------------------\n\n"
+        f"County Votes:\n"
     )
 
     # Print results and save to txt file
     print(election_results,end="")
     txt_file.write(election_results)
+
+    # Loop through al counties and calculate and print their percentage of the vote
+    for county in county_votes:
+
+        votes = county_votes[county]
+
+        vote_percentage = float(votes) / float(total_votes) * 100
+
+        county_results = (f"{county}: {vote_percentage:.1f}% ({votes:,})\n")
+
+        # Print county results and save to file
+        print(county_results)
+        txt_file.write(county_results)
+
+        # Determine the largest county turnout
+        if votes > largest_county_votes:
+
+            largest_county = county
+
+            largest_county_votes = votes
+    
+    # Establish largest county and formatting
+    county_summary = (
+        f"\n"
+        f"-------------------------\n"
+        f"Largest County Turnout: {largest_county}\n"
+        f"-------------------------\n"
+
+    )
+
+    # Print and save largest county with 
+    print(county_summary)
+    txt_file.write(county_summary)
 
     # Loop through all candidates and calculate and print their percentage of the vote
     for candidate_name in candidate_options:
